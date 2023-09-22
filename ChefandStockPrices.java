45ddab2899f31d84411e7e07fee74a49d8e9d5a6
danny boi <43886538+danushv@users.noreply.github.com>
2023-09-22 18:14:47 -0500

    1069 own

---
+++ ChefandStockPrices.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input =new Scanner(System.in);
+		int t=input.nextInt();
+		while(t-->0)
+		{
+		    double S=input.nextDouble();
+		    double A=input.nextDouble();
+		    double B=input.nextDouble();
+		    double C=input.nextDouble();
+		    
+		    double result=(S+((S*C)/100));
+		    if(result>=A && result<=B)
+		    {
+		      System.out.println("Yes");
+		    }
+		    else{
+		        System.out.println("No");
+		    }
+		}
+	}
+}
+// 
